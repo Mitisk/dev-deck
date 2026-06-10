0655599b982f3403938f22abbc37e8dfac1f9afc
Mitisk <masterhof@mail.ru>
2026-06-10 20:58:51 +0300

    feat(backend): creds_reorder command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -152,6 +152,7 @@ pub fn run() {
             commands::creds::creds_create,
             commands::creds::creds_update,
             commands::creds::creds_delete,
+            commands::creds::creds_reorder,
             commands::notes::notes_list,
             commands::notes::notes_create,
             commands::notes::notes_update,
